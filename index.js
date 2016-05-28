e542d0793f856170eef8bae2cc014571288cd838
Tuna Onder <Onder@Tunas-MacBook-Pro.local>
2016-05-28 17:58:45 +0300

    push 17

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var api = new ParseServer({
       apiKey: '' // The Server API Key of GCM
     },
     ios: {
-      pfx: '/certs/ParsePushDevelopmentCertificate.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk
+      pfx: '/certs/PushCertificate.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk
       bundleId: 'com.tunaonder.BilkentRide', // The bundle identifier associate with your app
       production: false // Specifies which environment to connect to: Production (if true) or Sandbox
     }
