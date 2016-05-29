9172d85bfecfbf3112070613e0afa98d3824c018
Tuna Onder <Onder@Tunas-MacBook-Pro.local>
2016-05-29 12:35:59 +0300

    push test

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  push: {
-    
+  push: {    
     ios: {
       pfx: __dirname + '/cert/PushCertificate.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk
       bundleId: 'com.tunaonder.BilkentRide', // The bundle identifier associate with your app
