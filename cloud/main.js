1665e559856f9fe60d80fbb00a6611ed92c6008c
tunaonder <saito@vt.edu>
2016-07-05 11:51:20 +0300

    push developing

--- cloud/main.js
+++ cloud/main.js
@@ -6,26 +6,6 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define("pushToAll", function(request, response) {
 
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  // pushQuery.containedIn("deviceType", ["ios", "android"]); // errors if no iOS certificate
-  // Send push notification to query
-  Parse.Push.send({
-    where: pushQuery, // Set our installation query
-    data: {
-      alert: 'Push test!!'
-    }
-  }, {
-    success: function() {
-      // Push was successful
-      console.log("Message was sent successfully");
-      response.success('true');
-    },
-    error: function(error) {
-      response.error(error);
-    },
-    useMasterKey: true
-  }); 
   // With promises
 Parse.Push.send({
   where: { "deviceType": {
