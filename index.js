ab8bd182bcd2fe81fe4ec7e03e2bfbd75bf4c6be
tunaonder <saito@vt.edu>
2016-09-18 20:12:22 -0400

    push problem 2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'uniride19544bWDklwejg',
   masterKey: process.env.MASTER_KEY || 'aeiWIFSKDSLF4W32123JFKAsd', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://uniride.herokuapp.com',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://uniride.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
