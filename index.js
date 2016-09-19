56077dc76dea4c3b0db368dbed63d0df06381aa9
tunaonder <saito@vt.edu>
2016-09-18 21:56:14 -0400

    couldnt solve push problem

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
 
-var app = express();
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -37,16 +33,19 @@ var api = new ParseServer({
 
 });
 
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
 
-app.use('/parse', api);
+var app = express();
+
 
 // Serve static assets from the /public folder
-//app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-//var mountPath = process.env.PARSE_MOUNT || '/parse';
-//var mountPath = '/parse';
-//app.use(mountPath, api);
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
 
 
 
